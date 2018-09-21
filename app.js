60870dce4b92ef0df6c0fb17b23e18d2251e4cd5
lgrees <lukasgrees@gmail.com>
2018-09-21 11:07:28 -0500

    added models to our db

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-const volleyball = require("volleyball");
-const express = require("express");
-const { db } = require('./models');
-const views = require("./views/index")
+const volleyball = require('volleyball');
+const express = require('express');
+const models = require('./models');
+const views = require('./views/index');
 const app = express();
 const PORT = 3000;
 
 app.use(volleyball);
-app.use(express.static(__dirname + "/public/stylesheets"));
-app.use(express.urlencoded( {extended: false} ));
+app.use(express.static(__dirname + '/public/stylesheets'));
+app.use(express.urlencoded({ extended: false }));
 
-db.authenticate().
-then(() => {
+models.db.authenticate().then(() => {
   console.log('connected to the database');
-})
-
-app.get( "/", (req, res) => {
-  res.send(views.main())
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-})
+app.get('/', (req, res) => {
+  res.send(views.main());
+});
 
+const init = async () => {
+  await models.db.sync({ force: true });
+  //await models.User.sync();
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+};
+
+init();
 
 //Lukas added a comment
