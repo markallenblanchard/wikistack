e82cd4f0dca2015a84b73d01c26865858e37291f
Mark B <markallenblanchard@gmail.com>
2018-09-21 15:07:31 -0500

    dynamic routing with params functional!

--- models/index.js
+++ models/index.js
@@ -1,6 +1,13 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost/wikistack', { logging: false });
 
+function createSlug(title) {
+  var regex = / /gi;
+  // Removes all non-alphanumeric characters from title
+  // And make whitespace underscore
+  return title.replace(/\s+/g, '_').replace(/\W/g, '');
+}
+
 const Page = db.define('page', {
   title: {
     type: Sequelize.STRING,
@@ -18,6 +25,10 @@ const Page = db.define('page', {
     type: Sequelize.ENUM('open', 'closed'),
   },
 });
+
+Page.beforeValidate( (page) => {
+  page.slug = createSlug(page.title)
+})
 
 const User = db.define('user', {
   name: {
